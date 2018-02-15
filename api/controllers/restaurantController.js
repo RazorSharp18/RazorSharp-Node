508606a03ba820984cf9302fe8b68e1431f985a9
AnveshkumarKolluri <anveshkolluri93@gmail.com>
2018-02-14 22:21:21 -0600

    Added getRestaurantByName and getRestaurantNames services

--- api/controllers/restaurantController.js
+++ api/controllers/restaurantController.js
@@ -5,8 +5,24 @@ var ObjectID = require('mongodb').ObjectID;
 var mongoose = require('mongoose'),
   Restaurants = mongoose.model('Restaurants');
 
+  exports.list_all_restaurant_names = function(req, res) {
+    Restaurants.find({}, {_id: 0, name: 1}, function(err, restaurant) {
+      if (err)
+        res.send(err);
+      res.json(restaurant);
+    });
+  };
+
 exports.list_all_restaurants = function(req, res) {
   Restaurants.find({}, function(err, restaurant) {
+    if (err)
+      res.send(err);
+    res.json(restaurant);
+  });
+};
+
+exports.get_restaurant_by_name = function(req, res) {
+  Restaurants.findOne({name: req.params.name}, function(err, restaurant) {
     if (err)
       res.send(err);
     res.json(restaurant);
@@ -23,7 +39,6 @@ exports.save_restaurant = function(req, res) {
 };
 
 exports.remove_restaurant = function(req, res) {
-  
   Restaurants.remove({
     _id: new ObjectID(req.params.restaurant_id)
   }, function(err, task) {
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express'),
   app = express(),
-  port = process.env.PORT || 3000;
+  port = process.env.PORT || 3001;
   mongoose = require('mongoose'),
   Restaurant = require('./api/models/restaurantModel'), //created model loading here
   bodyParser = require('body-parser');
@@ -12,7 +12,7 @@ mongoose.connect('mongodb://localhost/RazorLive');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-var routes = require('./api/routes/restauranRoutes'); //importing route
+var routes = require('./api/routes/restaurantRoutes'); //importing route
 routes(app); //register the route
 
 
